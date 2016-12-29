c7c36d9ace9be733c1b1b9f0c28955b355d55257
max <max@nix>
2016-12-29 01:50:14 +0100

    updated AssemblyInfo

--- src/Autofac.Extras.Plugins/Properties/AssemblyInfo.cs
+++ src/Autofac.Extras.Plugins/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Autofac.Extras.Plugins")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Makes it easy to create a plugin-based architecture with Autofac.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Autofac.Extras.Plugins")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Autofac.Extras.Plugins contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
